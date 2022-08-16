7b02b3c3600b460f3da3e674bbd74da4f234f49d
Ubuntu <ubuntu@ip-172-31-35-151.us-east-2.compute.internal>
2022-08-16 00:19:21 +0000

    front end

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const app = express();
-const port = 3003;
+const bodyParser = require("body-parser");
+const port = 5000;
+
+// app.get("/", (req, res) => {
+//   res.send("Hello Lab!!");
+// });
 
 app.get("/", (req, res) => {
-  res.send("Hello Lab!");
+  res.sendFile(__dirname + "/index.html");
 });
 
+// serve your css as static
+app.use(express.static(__dirname));
+
+// get our app to use body parser 
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//post
+
+app.post("/", (req, res) => {
+  var subName = req.body.yourname
+  var subEmail = req.body.youremail;
+ res.send(`Hello ${subName}! Thank you for subscribing. Your email is ${subEmail}.`);
+});
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Lab app listening on port ${port}`);
 });
